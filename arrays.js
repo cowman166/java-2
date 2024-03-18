b4b072f8e86ab6ed4ed7e9f54733a03ec1678556
Andre Montgomery <andremontgomery166@gmail.com>
2024-03-18 16:02:52 -0700

    workkkkin

--- arrays.js
+++ arrays.js
@@ -3,14 +3,28 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+  for (const i in items) {
+    console.log("${items[i]} ${i}");
+  }
 }
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+  const result = [];
+  for (const i in items.length){
+    if (i % 2 === 0){
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
 }
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
   // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b);
+  const sortedNItems = sortedItems.slice(0, n).reverse();
+
+  console.log(sortedNItems);
 }
